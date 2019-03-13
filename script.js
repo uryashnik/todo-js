15bdbafa8d1f5f5c82faf01269c600fdc5820527
Vitaliy <uryashnik@gmail.com>
2019-03-14 01:43:23 +0200

    com

--- script.js
+++ script.js
@@ -4,23 +4,36 @@ const inputItem = document.getElementById('inputItem');
 const btn = document.getElementById('btn');
 let id = store.length;
 
+
+function renderTask(task) {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    div.className = 'div-todo-task';
+    span.innerHTML = task;
+    const btnDel = document.createElement('button');
+    btnDel.innerText = 'Delete';
+    btnDel.className = 'btn-delete';
+    div.appendChild(span);
+    div.appendChild(btnDel);
+    let list = document.getElementById('list');
+    list.appendChild(div);
+}
 //Добавление новой записи в хранилище.
-function addTaskToStore(){
-    const inputValue = document.getElementById('inputItem').value;
-    if(inputValue.length > 0){
-        const storeItem = {todoName: inputValue, cheked: false, id}
+function addTaskToStore() {
+    const inputValue = inputItem.value;
+    if (inputValue.length > 0) {
+        const storeItem = { todoName: inputValue, cheked: false, id }
         const newArr = [...store, storeItem];
         store = newArr;
         id++;
         console.log(store);
+        renderTask(inputValue);
         document.getElementById('inputItem').value = '';
-    }else{
+    } else {
         alert('Укажите название задачи')
-    }
+    };
 };
 
-function renderTask(){
-    
-}
+
 
 btn.addEventListener('click', addTaskToStore);
